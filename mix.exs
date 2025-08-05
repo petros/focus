b0b1285c3ef8144240963ab7a469c3bc3adfe7c4
Petros Amoiridis <petros@hey.com>
2025-08-05 19:20:00 +0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Focus.MixProject do
   def project do
     [
       app: :focus,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
