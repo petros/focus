2164f73ea7a7d410edd98a8bf7051bbeee7a4730
Petros Amoiridis <petros@hey.com>
2025-08-07 14:11:01 +0300

    Fix executable output not showing

--- lib/focus/timer.ex
+++ lib/focus/timer.ex
@@ -13,17 +13,20 @@ defmodule Focus.Timer do
     
     IO.puts("\n🎉 Time's up!")
     Focus.Sound.play_notification()
+    
+    # Properly terminate the application for CLI usage
+    System.halt(0)
   end
 
   defp countdown(0) do
-    IO.write("\r00:00")
+    IO.write(:stdio, "\r00:00")
   end
 
   defp countdown(seconds) do
     minutes = div(seconds, 60)
     remaining_seconds = rem(seconds, 60)
     
-    IO.write("\r#{format_time(minutes)}:#{format_time(remaining_seconds)}")
+    IO.write(:stdio, "\r#{format_time(minutes)}:#{format_time(remaining_seconds)}")
     
     :timer.sleep(1000)
     countdown(seconds - 1)
